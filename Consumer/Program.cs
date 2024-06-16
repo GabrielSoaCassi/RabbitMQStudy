b0a8cc80298e8482ad48051112bc1af9e8e58dbe
Gabriel Cassimiro <gsoarescassimiro@gmail.com>
2024-06-16 02:21:55 -0300

    Adicionar arquivos de projeto.

---
+++ Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Servidor do Rabbitmq
+var servidor = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 0,
+    UserName = "",
+    Password = ""
+};
+
+//Conexão com o servidor
+var conexao = servidor.CreateConnection();
+
+/* 
+ * var conexao = servidor.CreateConnection();
+ * IModel channel = conexao.CreateModel();
+ * channel.Close();
+ * conexao.Close(); 
+ */
+//Canal usamos o using para não ter a necessidade de chamar o metodo close
+using (var canal = conexao.CreateModel())
+{
+    //escutar fila
+    canal.QueueDeclare(
+        queue: "",
+        durable: false, 
+        exclusive: false, 
+        autoDelete: false, 
+        arguments: null);
+
+    //Consumidor
+    var consumidor = new EventingBasicConsumer(canal);
+    //evento que o consumidor está escutando 
+    consumidor.Received += (model, ea) =>
+    {
+        //vamos receber o corpo do rabbit
+        var corpo = ea.Body.ToArray();
+        //pegar a mensagem que está em bytes e converter
+        var mensagem = Encoding.UTF8.GetString(corpo);
+        //mostrando a mensagem
+        Console.WriteLine(" [x] Recebido {0}", mensagem);
+    };
+    canal.BasicConsume(queue:"",autoAck:true,consumer:consumidor);
+
+}
+Console.WriteLine("Pressione [enter] para sair.");
+Console.ReadKey();
---
+++ Publisher/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Servidor do Rabbitmq
+var servidor = new ConnectionFactory()
+{
+    HostName = "localhost",
+    Port = 0,
+    UserName = "",
+    Password = ""
+};
+
+//Conexão com o servidor
+var conexao = servidor.CreateConnection();
+//Canal
+using (var canal = conexao.CreateModel())
+{
+    //Criar fila ou escutar fila
+
+    canal.QueueDeclare(
+        queue: "",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    //Mensagem
+    string mensagem = "Hello World";
+    //Converter mensagem para bytes
+    var corpoMensagem = Encoding.UTF8.GetBytes(mensagem);
+    canal.BasicPublish(
+        exchange: "",
+        routingKey: "",
+        basicProperties: null,
+        body: corpoMensagem);
+    Console.WriteLine(" [x] Enviou {0}", mensagem);
+}
+Console.WriteLine("Pressione [enter] para sair.");
+Console.ReadKey();
